cb2aa019df3c5902b67faefcb86b35e36ea14cf9
AmrMansour94 <amr.mansour6644@gmail.com>
2022-09-05 20:46:20 +0200

    Finish Creating Controllers

---
+++ e-Tickets/Controllers/CenimasController.cs
@@ -0,0 +1,24 @@
+﻿using e_Tickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_Tickets.Controllers
+{
+    public class CenimasController : Controller
+    {
+        private readonly AppDbContext _ctx;
+        public CenimasController(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var cenimas = await _ctx.Cenimas.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ e-Tickets/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using e_Tickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_Tickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _ctx;
+        public MoviesController(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _ctx.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
